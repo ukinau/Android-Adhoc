fd0a0184960d7ff3f43c98e1d74e364cee8b515d
uckey <uckey.1067@gmail.com>
2013-06-23 22:32:16 +0900

    Add Constructor receiving  custom function after receive udp packet

--- src/android/tether/udp/UdpReceiveThread.java
+++ src/android/tether/udp/UdpReceiveThread.java
@@ -3,11 +3,14 @@ package android.tether.udp;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketAddress;
 import java.net.SocketException;
 
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.tether.dtn.ReceivedBehaver;
 import android.tether.system.AndroidTetherConstants;
 import android.util.Log;
 import android.widget.Toast;
@@ -21,11 +24,18 @@ import android.widget.Toast;
 public class UdpReceiveThread extends Thread {
 	public DatagramSocket recSocket;
 	public Handler handler;
+	public ReceivedBehaver behaver;
 	public boolean socketOpen;
 	public UdpReceiveThread(int portNumber , Handler handle) throws SocketException{
 		recSocket = new DatagramSocket(portNumber);
 		socketOpen = true;
 		this.handler = handle;
+	}
+	
+	public UdpReceiveThread(int portNumber , ReceivedBehaver behaver) throws SocketException{
+		recSocket = new DatagramSocket(portNumber);
+		socketOpen = true;
+		this.behaver = behaver;
 	}
 	
 	@Override
@@ -34,13 +44,18 @@ public class UdpReceiveThread extends Thread {
 			try{
 				String res = receive();
 				
-				// reflect the result received
-				Message msg = new Message();
-				Bundle data=new Bundle();
-				data.putString("msg", res);
-				data.putString("protocol", "UDP" );
-				msg.setData(data);
-				handler.sendMessage(msg);
+				if(this.handler != null){
+					// reflect the result received
+					Message msg = new Message();
+					Bundle data=new Bundle();
+					data.putString("msg", res);
+					data.putString("protocol", "UDP" );
+					msg.setData(data);
+					handler.sendMessage(msg);
+				}
+				if(this.behaver != null){
+					behaver.after_packet_received("","");
+				}
 				
 			}catch(Exception e){
 				e.printStackTrace();
@@ -58,9 +73,11 @@ public class UdpReceiveThread extends Thread {
 		byte []buf = new byte[AndroidTetherConstants.UDP_BUFFER_SIZE];
 		DatagramPacket packet= new DatagramPacket(buf,buf.length);
 		recSocket.receive(packet);//receive & wait
+		SocketAddress macAddress = packet.getSocketAddress();
+		macAddress.toString();
 		int len = packet.getLength();
 		String msg = new String(buf, 0, len);
-		return msg;
+		return msg+" mac:"+macAddress.toString();
 	}
 
 }
