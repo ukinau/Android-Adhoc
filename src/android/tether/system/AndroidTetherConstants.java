83f562d0bca0e6b89b9e0307368e6688fc75a9c3
uckey <uckey.1067@gmail.com>
2013-06-23 22:31:39 +0900

    Increase udp buffer size

--- src/android/tether/system/AndroidTetherConstants.java
+++ src/android/tether/system/AndroidTetherConstants.java
@@ -5,7 +5,7 @@ public class AndroidTetherConstants {
 	public final static int CHAT_TCP_SERVER_PORT = 3001;
 	public final static int DTN_UDP_RECEIVE_PORT = 3100;
 	public final static int DTN_TCP_SERVER_PORT = 31001;
-	public final static int UDP_BUFFER_SIZE = 256;
+	public final static int UDP_BUFFER_SIZE = 10000;
 	public final static int TCP_BUFFER_SIZE = 256;
 	public final static String subnet = "192.168.2";
 }
