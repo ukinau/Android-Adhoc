5c8ca6822226fd90f746a7392c5010608e2fa469
uckey <uckey.1067@gmail.com>
2013-07-09 16:55:50 +0900

    バッファサイズ変更

--- src/android/tether/system/AndroidTetherConstants.java
+++ src/android/tether/system/AndroidTetherConstants.java
@@ -6,6 +6,6 @@ public class AndroidTetherConstants {
 	public final static int DTN_UDP_RECEIVE_PORT = 3100;
 	public final static int DTN_TCP_SERVER_PORT = 31001;
 	public final static int UDP_BUFFER_SIZE = 10000;
-	public final static int TCP_BUFFER_SIZE = 256;
+	public final static int TCP_BUFFER_SIZE = 10000; 
 	public final static String subnet = "192.168.2";
 }
